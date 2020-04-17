f5bb6edb289aa11896c07b9861e92fa7af15615c
infinitycoding222 <58121837+infinitycoding222@users.noreply.github.com>
2020-04-17 22:15:33 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./ElectroClient.js', { token: 'Njk0MjE0Nzg3MjI3NDUxNDky.XoXpNg.4BmHQkJ7RytMsfzL3R0pmvSJWww' });
+const manager = new ShardingManager('./ElectroClient.js', { token: 'shit' });
 
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched Shard ${shard.id}`));

e36833fa5f135ce1f89d51a837e78a251a48c7d2
infinitycoding222 <58121837+infinitycoding222@users.noreply.github.com>
2020-04-17 22:15:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.token = 'Njk0MjE0Nzg3MjI3NDUxNDky.XoXpNg.4BmHQkJ7RytMsfzL3R0pmvSJWww',
-    exports.owners = ["473276250815856650"],
-    exports.prefix = 'v'
+exports.token = 'Ntoken',
+    exports.owners = ["id"],
+    exports.prefix = 'prefix'
